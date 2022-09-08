94ac1107d3f9b1422405420f23a0104fcdea2eb0
Chase <cverbout@gmail.com>
2022-09-08 11:22:34 -0700

    Responsive edits to gallery, blog, and wcv+footer

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -25,19 +25,19 @@ const Blog = () => {
           </div>
         </div>
         <div className='mw__mam-content_infobox'>
-          <div className='mw__mam-content_infobox_bio'>
-            
+          {/* <div className='mw__mam-content_infobox_bio'>
             <p>
-              After graduating from the Univesity of Washington I spent my summer finishing my internship at KCM. I spent the next year acclimating further to the Computer Science program at Portland State. This January I will begin their Computer Sceince Master's Degree program.
+              I have spent the past year taking courses within the Computer Science program at Portland State and working on personal projects. This January I will begin PSU's Computer Sceince Master's Degree program.
             </p>
-          </div>
+          </div> */}
           <ul className='mw__mam-content_infobox_list'>
             <h3>Hobbies</h3>
-            <li>I enjoy a long uphill hike- some of my past favorites are Mt. St. Helens and Mt. Adams</li>
-            <li >Huge Trail Blazers and Seahawks fan<b>#DameTime</b></li>
-            <li>I DM for my friends- we are currently running the LMOP campaign</li>
+
+            <li><Icon className='mw__mam-content_icon' icon="la:hiking" width="48" height="48" />Big hiker- really enjoyed Mt. St. Helens and Mt. Adams</li>
+            <li><Icon className='mw__mam-content_icon' icon="carbon:basketball" width="48" height="48" />Lifelong Trail Blazers fan and a Seahawks fan #DameTime</li>
+            <li><Icon className='mw__mam-content_icon' icon="la:dice-d20" width="48" height="48" />I DM for my friends- currently running the LMOP campaign</li>
             <h3>Recomendations</h3>
-            <li><Icon className='mw__mam-content_icon' icon="bi:controller" width="48" height="48" />Play:&nbsp;<b>Bioshock Infinite/Shadow of the Collossus</b></li>
+            <li><Icon className='mw__mam-content_icon' icon="bi:controller" width="48" height="48" />Play:&nbsp;<b>Bioshock Infinite</b></li>
             <li><Icon className='mw__mam-content_icon' icon="akar-icons:book-open" width="48" height="48" />Read:&nbsp;<b>The Name of the Wind</b></li>
             <li><Icon className='mw__mam-content_icon' icon="eva:tv-outline" width="48" height="48" />Watch:&nbsp;<b>Hunter x Hunter</b></li>
           </ul>
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import "./footer.css";
+import logoA from '../../assets/logoA.png';
+
+const Menu = () => (
+  <React.Fragment>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#wcv'>Who is Chase V?</a></p>
+    <p><a href='#prj'>Projects</a></p>
+    <p><a href='#qual'>Qualifications</a></p>
+    <p><a href='#res/cm'>Resume</a></p>
+    <p><a href='#res/cm'>Contact Me</a></p>
+    <p><a href='#mam'>More about me</a></p>
+  </React.Fragment>
+)
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='mw__footer'>
+      <div className='mw__footer-content'>
+        <div className='mw__footer-name'>
+          <img src={logoA} alt="logo"/>
+          <h1>Chase Verbout</h1>
+        </div>
+        <div className='mw__footer-copyright'>
+          <h3>Copyright © 2022</h3>
+        </div>
+        <div className='mw__footer-links'>          
+          <ul>
+            <Menu></Menu>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
