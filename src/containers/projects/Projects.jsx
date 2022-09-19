b05aa619fa56e5291a2710798e70822fe8a29b12
Chase <cverbout@gmail.com>
2022-09-19 13:02:03 -0700

    css formating and picture additions

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -1,21 +1,31 @@
 import React, {Component} from 'react';
 import "./projects.css";
 import {Project} from '../../components';
+
 import Character1 from '../../assets/character-1.png';
-import Enemy1 from '../../assets/enemy-1.png';
-import Creature1 from '../../assets/creature-1.png';
 import Character2 from '../../assets/character-2.png';
-import Enemy2 from '../../assets/enemy-2.png';
-import Creature2 from '../../assets/creature-2.png';
-import Backend1 from '../../assets/backend-img-1.jpg';
-import Backend2 from '../../assets/backend-img-2.jpg';
+import Enemy1 from '../../assets/enemy-slime.png';
+import Enemy2 from '../../assets/enemy-slimeG.png';
+import Enemy3 from '../../assets/enemy-slimeR.png';
+import Enemy4 from '../../assets/enemy-slimeP.png';
+import Enemy5 from '../../assets/enemy-slurtle.png';
+import Enemy6 from '../../assets/enemy-slurtleR.png';
+import Enemy7 from '../../assets/enemy-boss.png';
+import Creature1 from '../../assets/creature-druppy.png';
+import Creature2 from '../../assets/creature-druppyB.png';
+import Creature3 from '../../assets/creature-druppyG.png';
+import Creature4 from '../../assets/creature-druppyR.png';
+
 import HomepageSmall from '../../assets/webpage_home_small.jpg';
 import HomepageSmallMenu from '../../assets/webpage_home_small_menu.jpg';
 import Qual from '../../assets/webpage_qual.jpg';
 import MamFoot from '../../assets/webpage_mam_footer.jpg';
 import Logo from '../../assets/logo.png';
-import LogoA from '../../assets/logoA.png';
+import LogoA from '../../assets/alternateLogo.png';
 
+import Backend1 from '../../assets/backend-img-1.jpg';
+import Backend2 from '../../assets/backend-img-2.jpg';
+import WIP from '../../assets/WIP.png'
 
 class Projects extends Component {
 
@@ -26,19 +36,31 @@ class Projects extends Component {
         {id: 3, url: Logo},
         {id: 4, url: LogoA},
         {id: 5, url: Qual},
-        {id: 6, url: MamFoot},
-        {id: 7, url: Creature2}
+        {id: 6, url: MamFoot}
     ],
 
     project2: [
-      {id: 1, url: Backend1},
-      {id: 2, url: Character1},
+      {id: 1, url: Character1},
+      {id: 2, url: Character2},
       {id: 3, url: Enemy1},
-      {id: 4, url: Creature1},
-      {id: 5, url: Character2},
-      {id: 6, url: Enemy2},
-      {id: 7, url: Creature2}
+      {id: 4, url: Enemy2},
+      {id: 5, url: Enemy3},
+      {id: 6, url: Enemy4},
+      {id: 7, url: Enemy5},
+      {id: 8, url: Enemy6},
+      {id: 9, url: Enemy7},
+      {id: 10, url: Creature1},
+      {id: 10, url: Creature2},
+      {id: 10, url: Creature3},
+      {id: 10, url: Creature4}
   ],
+
+  project3: [
+    {id: 1, url: WIP},
+    {id: 2, url: WIP},
+    {id: 3, url: WIP},
+    {id: 4, url: WIP}
+],
 
     tools1: [
       {Name: "JSX"},
@@ -91,7 +113,7 @@ class Projects extends Component {
             name={"RoadHero"}
             href={"https://unity.com/"}
             description={"My first attempt at creating a playable game with a loose plot and clear objective. RoadHero is a 2d side scroller that shovels a slew of enemies with varying design, speed, and health each round. You, along with your trusty Droggo, must relentlesssly fling fireballs to hold back the onslaught for 14 rounds before facing the horde leader on round 15. I designed the characters myself and made use of the Unity Asset Store for other features."}
-            pictures = {this.state.project1}
+            pictures = {this.state.project2}
             tools = {this.state.tools2}
             color_style = {{color: 'rgb(72, 150, 228)'}}
             bg_style = {{background: 'rgb(72, 150, 228)'}}
@@ -100,7 +122,7 @@ class Projects extends Component {
             <Project  
             name={"Snaggle (In-Progress)"}
             description={"Snaggle is a full stack web application that provides bug tracking services. The backend is a combination ASP.NET and SQL Server while the front-end was made with React."}
-            pictures = {this.state.project1}
+            pictures = {this.state.project3}
             tools = {this.state.tools3}
             color_style = {{color: '#2BB4D3'}}
             bg_style = {{background: '#2BB4D3'}}
